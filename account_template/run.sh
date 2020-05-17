155faa6884e531cd249e9ab4f69f3e95ee57906f
Kashirin Alex <kashirin.alex@gmail.com>
2020-05-17 12:46:05 +0300

    initial commit
    Signed-off-by: Kashirin Alex <kashirin.alex@gmail.com>

--- account_template/run.sh
+++ account_template/run.sh
@@ -2,13 +2,15 @@
 
 
 # CONFIG
-DIR=$PWD;
 CA_JKS_PASS=;
 
 ACC_TYPE=demo; # live/demo
 ACC_USR=;
 ACC_PWD=;
 
+
 # RUN
-nohup java -Dcache_dir=$DIR/.cache -DUSR=$ACC_USR -DPWD=$ACC_PWD -Djavax.net.ssl.trustStore=$DIR/../cacerts.jks -Djavax.net.ssl.trustStorePassword=$CA_JKS_PASS -cp $DIR/../project/target/price-deviation-1.0-bundled.jar strategies.PriceDeviation &> logs/out &
+nohup java -Dcache_dir=.cache -DUSR=$ACC_USR -DPWD=$ACC_PWD -Djavax.net.ssl.trustStore=../cacerts.jks -Djavax.net.ssl.trustStorePassword=$CA_JKS_PASS -cp price-deviation.jar strategies.PriceDeviation &> logs/out &
+
+sleep 1;
 tail logs/out;
--- build.sh
+++ build.sh
@@ -4,9 +4,15 @@
 mvn -f project/pom.xml -Dmaven.test.skip=true package;
 
 
-# INIT_TEST_ACC
+
+# MOVE CLASS TO ACC
+
 if [ -z $ACC_NAME ]; then
+    # INIT_TEST_ACC
     ACC_NAME=DEMO_TEST_1;
 fi
+
 mkdir $ACC_NAME;
-cp -r account_template $ACC_NAME;
+cp -r account_template/* $ACC_NAME;
+cp project/target/price-deviation-1.0-bundled.jar $ACC_NAME/price-deviation.jar;
+
